44f1a7f15240b5a3020876cff17242efc5922e14
Enoon <fabiom.carlucci@yahoo.it>
2015-07-17 16:33:34 +0200

    Setup for testing both P and Lambda
    Setup for testing both P and Lambda

--- nbnl/gridJobInterpreter.m
+++ nbnl/gridJobInterpreter.m
@@ -1,14 +1,20 @@
-function [ lambda, input_folder, split ] = gridJobInterpreter( jobId )
+function [p, lambda, input_folder, split ] = gridJobInterpreter( jobId )
 %JOBINTERPRETER Summary of this function goes here
 %   Detailed explanation goes here
-    nLambda = 30;
-    lambda_range =  linspace(1e-9,1e-5,nLambda);
-    index = mod(jobId,nLambda);
+    ps = [1:0.1:2 3 10 1000];
+    lambdas = [1e-07 1e-06 1e-05 1e-04];
+    nP = length(ps);
+    nLambda = length(lambdas);
+    blockSize = nP*nLambda;
+    
+    index = mod(jobId, blockSize);
     if index==0
-        index = nLambda;
+        index = blockSize;
     end
-    lambda = lambda_range(index);
-    split = ceil(jobId/nLambda);
+    [pI lI] = ind2sub([nP nLambda],index);
+    lambda = lambdas(lI);
+    p = ps(pI);
+    split = ceil(jobId/blockSize);
     input_folder = 'all_32_3_base_hybrid_mean';
 end
 
