f11f2d90ea04ab37638747f150be5528937d5ca1
Fabio Maria Carlucci <fabiom.carlucci@yahoo.it>
2015-10-09 10:45:46 +0200

    31 classes job

--- DANBNN_demo/functions/gridJobInterpreter.m
+++ DANBNN_demo/functions/gridJobInterpreter.m
@@ -4,30 +4,30 @@ function [ params ] = gridJobInterpreter( jobId , dataDir)
 %   and 10 steps for training size, 2 for relu
     % get algorithm parameters
     params.relu = false;
-    categories = {'backpack.hdf5' 'headphones.hdf5' 'monitor.hdf5' 'bike.hdf5' 'keyboard.hdf5' 'mouse.hdf5' 'projector.hdf5' 'calculator.hdf5'  'laptop.hdf5' 'mug.hdf5'};
+    categories = {};%{'backpack.hdf5' 'headphones.hdf5' 'monitor.hdf5' 'bike.hdf5' 'keyboard.hdf5' 'mouse.hdf5' 'projector.hdf5' 'calculator.hdf5'  'laptop.hdf5' 'mug.hdf5'};
     params.categories = categories;
-    BLOCK = 48; % 3 patch size * 4 source * 4 target
+    BLOCK = 27; % 3 patch size * 3 source * 3 target
     patches = [16 32 64];
     levels = [1 2 3];
-    if(jobId>144)
+    if(jobId>81)
         params.relu = true;
-        jobId = jobId - 144;
+        jobId = jobId - 81;
     end
-    datasets = {'office/amazon' 'office/webcam' 'office/dslr' 'caltech10'};   
+    datasets = {'office/amazon' 'office/webcam' 'office/dslr'};  %'caltech10'};
     level = levels(ceil(jobId/BLOCK));
     jobMod = mod(jobId, BLOCK);
     jobMod(jobMod==0)=BLOCK;
-    MINIBLOCK = 16; % 4 source and 4 target
+    MINIBLOCK = 9; % 4 source and 4 target
     patch = patches(ceil(jobMod/MINIBLOCK));
     jobMod = mod(jobMod, MINIBLOCK);
     jobMod(jobMod==0)=MINIBLOCK;
-    MINIBLOCK = 4;
+    MINIBLOCK = 3;
     sourceD = datasets{ceil(jobMod/MINIBLOCK)};
     jobMod = mod(jobMod, MINIBLOCK);
     jobMod(jobMod==0)=MINIBLOCK;
     targetD = datasets{jobMod};
     % prepare params object
-    folderName = strcat('/all_',num2str(patch),'_',num2str(level),'_extra_hybrid_mean_aug/');
+    folderName = strcat('/all_',num2str(patch),'_',num2str(level),'_extra_hybrid_mean/');
     S.path = strcat(dataDir,'/desc/',sourceD,folderName);
     S.dataset = sourceD;
     S.indexes = getImageIDs(S.path, categories);
@@ -44,8 +44,8 @@ function [ params ] = gridJobInterpreter( jobId , dataDir)
     params.patchSize = patch;
     params.levels = level;
     params.supervised = false;
-    params.splits = 5;
-    %params.patchPercent = 0;
+    params.splits = 10;
+    %params.patchPercent = 0.5;
     params.addPos = false;
     params.posScale = 0.1;
     fprintf('%d %s - %s -> %s - - - %d: (Relu %s) %.2f%%\n',jobId, folderName,sourceD,targetD, trainingSamples, char(params.relu+48), 100);%params.patchPercent*100);
