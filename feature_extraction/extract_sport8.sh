9b2444985b30de3bab58ea4bafb5e36c9e4e9f62
enoonIT <fabiom.carlucci@yahoo.it>
2015-10-14 10:18:56 +0200

    added sport extraction file

---
+++ feature_extraction/extract_sport8.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# This code extracts DECAF feature descriptors from patches of scene15 dataset.
+# Configuration variables are set below (see extract_sports.sh for descriptions).
+# Usage: ./extract_sports8.sh /input/dir jobId /destination/dir
+
+patch_size=(16 32 64)
+levels=(1 2 3)
+#patch_method=(base extra extra)
+COMBINATIONS=$((3)) #number of combinations for each dataset
+DATA_DIR=$1
+network_data_dir=$DATA_DIR/network/
+i=$2
+PATCH_EXTRACTION_METHOD=extra #${patch_method[i]}
+DATASET=sport8
+PSIZE=${patch_size[$((i % COMBINATIONS))]}
+LEVEL=${levels[$((i / COMBINATIONS))]}
+INPUT_DIR=$DATA_DIR/images/$DATASET
+OUT_NAME=all_${PSIZE}_${LEVEL}_${PATCH_EXTRACTION_METHOD}_hybrid_mean_dense
+OUT_PARENT=$DATA_DIR
+if [ "$#" -gt 2 ]; then
+  OUT_PARENT=$3
+fi
+OUTPUT_DIR=${OUT_PARENT}/desc/$DATASET/$OUT_NAME
+
+echo $OUTPUT_DIR
+
+PATCHES_PER_IMAGE=500
+IMAGE_DIM=200
+DATA_SPLIT=-1 #-1 means all images, will ignore NUM_TEST and NUM_TRAIN
+NUM_TRAIN=100 #100
+NUM_TEST=100 #100
+DECAF_LAYER_NAME=fc7_cudanet_out
+
+
+for f in $INPUT_DIR/*; do
+  echo $f
+python ./src/extract.py --input-dir $f --output-dir $OUTPUT_DIR \
+       --patches $PATCHES_PER_IMAGE --patch-size $PSIZE \
+       --image-dim $IMAGE_DIM --descriptor DECAF \
+       --levels $LEVEL --split $DATA_SPLIT \
+       --num-train-images $NUM_TRAIN --num-test-images $NUM_TEST \
+       --layer-name $DECAF_LAYER_NAME --network-data-dir $network_data_dir \
+       --patch-method $PATCH_EXTRACTION_METHOD
+done
