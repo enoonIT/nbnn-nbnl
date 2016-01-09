316ff82b8a1c749e46aaac3e6246656f1f455251
Fabio Maria Carlucci <fabiom.carlucci@yahoo.it>
2016-01-09 16:15:05 +0100

    misc

--- matlab/apply_pca.m
+++ matlab/apply_pca.m
@@ -3,7 +3,7 @@ function reduced_data = apply_pca(whole_data, U, n_dims, xmean, xstd)
         for n_class=1:numel(whole_data)
         	for n_image=1:numel(whole_data{n_class})
 		    tmp = whole_data{n_class}{n_image}';
-		    n = size(tmp, 1)
+		    n = size(tmp, 1);
 		    tmp = (tmp - repmat(xmean,[n 1])) ./ repmat(xstd,[n 1]); 
                     reduced_data{n_class}{n_image} = tmp*Ured;
 	 	end
--- matlab/generate_reduced_data.m
+++ matlab/generate_reduced_data.m
@@ -1,4 +1,5 @@
-patch='/patches7';
+LEVEL=7;
+patch=strcat('/patches',num2str(LEVEL));
 datasets{1}.data = load_patches('~/data/desc/caltech10/all_32_3_extra_hybrid_mean/', patch);
 datasets{1}.name = 'caltech';
 datasets{2}.data = load_patches('~/data/desc/office/amazon10/', patch);
@@ -9,10 +10,14 @@ datasets{4}.data = load_patches('~/data/desc/office/webcam10/', patch);
 datasets{4}.name = 'webcam';
 elements=1:numel(datasets);
 DIMS=128;
+dir_name = strcat('dims_',num2str(DIMS),'_std_relu_level',num2str(LEVEL),'/');
+if not(exist(dir_name,'dir'))
+	mkdir(dir_name);
+end
 for idx=elements
-	disp 'Start cycle'
-	X = cell_to_matrix(datasets{idx}.data);
 	name = datasets{idx}.name;
+	fprintf('Start cycle for target %s\n', name)
+	X = cell_to_matrix(datasets{idx}.data);
 	to_apply = elements([1:idx-1 idx+1:end]);
 	[xmean, xstd, coeff, latent ] = getPCAMatrix(X);
 	variance_kept = sum(latent(1:DIMS))/sum(latent)
@@ -20,7 +25,7 @@ for idx=elements
 	for dest=to_apply
 		disp(datasets{dest}.name)
 		reduced_data = apply_pca(datasets{dest}.data, coeff, DIMS, xmean, xstd );
-		save(strcat(name,'_to_',datasets{dest}.name,'__std_lvl7'), 'reduced_data');
+		save(strcat(dir_name, name,'_to_',datasets{dest}.name), 'reduced_data');
 	end
-	save(strcat('variance',DIMS,'_std_lvl7'), 'variance_kept');
+	save(strcat(dir_name, 'variance_', name), 'variance_kept');
 end
